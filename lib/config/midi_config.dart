3e4ce42ed755b43e89b3c375c8c2c818e7e4d8e8
Paul Osinga <paulpwo@gmail.com>
2025-08-15 19:13:54 -0500

    feat: Expand MIDI configuration by adding additional pads with corresponding notes and labels

--- lib/config/midi_config.dart
+++ lib/config/midi_config.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+
 import '../models/midi_pad.dart';
 import 'app_theme.dart';
 
@@ -13,31 +14,115 @@ class MidiConfig extends ChangeNotifier {
   final List<MidiPad> _midiPads = [
     MidiPad(
       cc: 20,
-      note: 60,
-      label: 'UNDERGROUND',
-      subtitle: 'CC 20',
+      note: 21, // A0
+      label: 'Pad0',
+      subtitle: 'A0',
       color: AppTheme.errorColor,
     ),
     MidiPad(
       cc: 21,
-      note: 62,
-      label: 'MY UNDERGROUND',
-      subtitle: 'CC 21',
+      note: 22, // A#0/Bb0
+      label: 'Pad1',
+      subtitle: 'A#0',
       color: AppTheme.secondaryVariant,
     ),
     MidiPad(
       cc: 30,
-      note: 64,
-      label: 'BEAT LOOP',
-      subtitle: 'CC 30',
+      note: 23, // B0
+      label: 'Pad2',
+      subtitle: 'B0',
       color: AppTheme.primaryColor,
     ),
     MidiPad(
       cc: 31,
-      note: 65,
-      label: 'SYNC PAD',
-      subtitle: 'CC 31',
+      note: 24, // C1
+      label: 'Pad3',
+      subtitle: 'C1',
       color: AppTheme.infoColor,
+    ),
+    MidiPad(
+      cc: 40,
+      note: 25, // C#1/Db1
+      label: 'Pad4',
+      subtitle: 'C#1',
+      color: AppTheme.warningColor,
+    ),
+    MidiPad(
+      cc: 41,
+      note: 26, // D1
+      label: 'Pad5',
+      subtitle: 'D1',
+      color: AppTheme.successColor,
+    ),
+    MidiPad(
+      cc: 50,
+      note: 27, // D#1/Eb1
+      label: 'Pad6',
+      subtitle: 'D#1',
+      color: AppTheme.accentColor,
+    ),
+    MidiPad(
+      cc: 51,
+      note: 28, // E1
+      label: 'Pad7',
+      subtitle: 'E1',
+      color: AppTheme.accentVariant,
+    ),
+    MidiPad(
+      cc: 60,
+      note: 29, // F1
+      label: 'Pad8',
+      subtitle: 'F1',
+      color: AppTheme.primaryColor,
+    ),
+    MidiPad(
+      cc: 61,
+      note: 30, // F#1/Gb1
+      label: 'Pad9',
+      subtitle: 'F#1',
+      color: AppTheme.secondaryColor,
+    ),
+    MidiPad(
+      cc: 70,
+      note: 31, // G1
+      label: 'Pad10',
+      subtitle: 'G1',
+      color: AppTheme.infoColor,
+    ),
+    MidiPad(
+      cc: 71,
+      note: 32, // G#1/Ab1
+      label: 'Pad11',
+      subtitle: 'G#1',
+      color: AppTheme.warningColor,
+    ),
+    MidiPad(
+      cc: 80,
+      note: 33, // A1
+      label: 'Pad12',
+      subtitle: 'A1',
+      color: AppTheme.successColor,
+    ),
+    MidiPad(
+      cc: 81,
+      note: 34, // A#1/Bb1
+      label: 'Pad13',
+      subtitle: 'A#1',
+      color: AppTheme.errorColor,
+    ),
+    MidiPad(
+      cc: 90,
+      note: 35, // B1
+      label: 'Pad14',
+      subtitle: 'B1',
+      color: AppTheme.primaryVariant,
+    ),
+    MidiPad(
+      cc: 91,
+      note: 36, // C2
+      label: 'Pad15',
+      subtitle: 'C2',
+      color: AppTheme.secondaryVariant,
     ),
   ];
 
